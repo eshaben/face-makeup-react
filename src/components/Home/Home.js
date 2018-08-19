04aa5b1d78bd376eebcd46a7510361e72c0916cc
Erin Shaben <eshaben@icloud.com>
2018-08-19 17:43:12 -0600

    initial react router set up

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome!</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
